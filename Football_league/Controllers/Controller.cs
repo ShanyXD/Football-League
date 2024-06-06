2eb7de8034105a289e90faefd3ec8ca9e017bf35
ShanyXD <123731823+ShanyXD@users.noreply.github.com>
2024-06-06 11:41:00 +0300

    starting stuff

---
+++ Football_league/Controllers/Controller.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+public class TeamsController : Controller
+{
+    private readonly FootballLeagueContext _context;
+
+    public TeamsController(FootballLeagueContext context)
+    {
+        _context = context;
+    }
+
+    // Action methods for creating, reading, updating, and deleting teams
+}
+
+public class MatchesController : Controller
+{
+    private readonly FootballLeagueContext _context;
+
+    public MatchesController(FootballLeagueContext context)
+    {
+        _context = context;
+    }
+
+    // Action methods for creating, reading, updating, and deleting matches
+}
+
+public class StandingsController : Controller
+{
+    private readonly FootballLeagueContext _context;
+
+    public StandingsController(FootballLeagueContext context)
+    {
+        _context = context;
+    }
+
+    // Action methods for viewing standings
+}
+
